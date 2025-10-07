61ff2c2bc87f60117b71db40a769acf150c7a184
nour ben hnia <nourbenhnia619@gmail.com>
2025-10-07 23:08:42 +0200

    axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+function searchcity(city){
+    let apk="62b63b2t79f05a00f231dof1d4ddeeaa";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apk}&units=metric`;
+    console.log(apiUrl);
+
+}
 function searchsubmit(event){
     event.preventDefault();
     let searchinput=document.querySelector('#searchinput').value;
     let city=document.querySelector('#city');
     city.innerHTML=searchinput;
-    
+    searchcity(searchinput);
 
 }
 let searchform=document.querySelector('#searchform');
